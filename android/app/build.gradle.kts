f187d422a6ae5b95b2bd0349042a0d8245246819
thebikercrewuae <thebikercrewuae@gmail.com>
2026-05-13 16:02:40 +0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -66,8 +65,5 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.concurrent:concurrent-futures:1.3.0")
-}
-
     implementation("androidx.concurrent:concurrent-futures:1.3.0")
 }
